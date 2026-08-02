1dfb0edabe659746022ff7a6513936b66fd83432
Luiz <luizfernandogl12345@gmail.com>
2026-08-02 19:36:04 -0300

    Aprendendo ASYNC AWAIT junto com Promisses e aprendendo a encontrar caminhos

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,14 +1,29 @@
 import { PostModel } from "@/models/posts/post-model";
 import { PostRepository } from "./post-repository";
+import { resolve } from "path";
+import { readFile } from "fs/promises";
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+   ROOT_DIR,
+   "src",
+   "db",
+   "seed",
+   "posts.json",
+);
 
 export class JsonPostRepository implements PostRepository {
-   private async readFromDisk() {}
+   private async readFromDisk() {
+      const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
+      const parsedJson = JSON.parse(jsonContent);
+      return parsedJson.posts;
+   }
 
-   async findAll(): Promise<PostModel[]> {}
+   async findAll(): Promise<PostModel[]> {
+      return this.readFromDisk();
+   }
 }
 
 export const postRepository = new JsonPostRepository();
 
-console.log(ROOT_DIR);
+postRepository.findAll().then((jsonContent) => console.log(jsonContent));
