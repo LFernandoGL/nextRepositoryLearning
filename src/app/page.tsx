226cb70010ed7fe6ff3d7eaef0b0e9cf86abf694
Luiz <luizfernandogl12345@gmail.com>
2026-08-05 16:41:24 -0300

    adicao de mais alguns componentes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 // import { Header } from "@/components/Header/Index";
 import { Container } from "@/components/Container/Index";
 import { Header } from "@/components/Header/Index";
+import { PostCoverImage } from "@/components/PostCoverImage/Index";
+import { PostHeading } from "@/components/PostHeading/Index";
 import { PostList } from "@/components/PostList/Index";
 import { SpinLoader } from "@/components/SpinLoader";
-import Image from "next/image";
+
 import Link from "next/link";
 import { Suspense } from "react";
 
@@ -13,20 +15,36 @@ export default async function HomePage() {
          <Header />
 
          <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-            <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
-               <Image
-                  className="group-hover:scale-105 transition"
-                  src="/images/bryen_0.png"
-                  alt="Titulo do Post"
-                  width={1200}
-                  height={720}
-               />
-            </Link>
-            <div>
-               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores
-               delectus maxime enim alias obcaecati eaque aliquam impedit cum
-               quasi sit aspernatur eum blanditiis doloribus, distinctio minima,
-               repellendus, nemo esse expedita?
+            <PostCoverImage
+               linkProps={{ href: "#" }}
+               imageProps={{
+                  src: "/images/bryen_5.png",
+                  alt: "imagem da minha pagina",
+                  width: 1200,
+                  height: 720,
+               }}
+            />
+
+            <div className="flex flex-col gap-4 justify-center">
+               <time
+                  className="text-slate-600 text-sm/tight block"
+                  dateTime="2026-08-05"
+               >
+                  05/08/2026 15:00
+               </time>
+
+               <PostHeading url="#">
+                  <p>
+                     Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  </p>
+               </PostHeading>
+
+               <p>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                  Maiores delectus maxime enim alias obcaecati eaque aliquam
+                  impedit cum quasi sit aspernatur eum blanditiis doloribus,
+                  distinctio minima, repellendus, nemo esse expedita?
+               </p>
             </div>
          </section>
          <Suspense fallback={<SpinLoader />}>
